811d49ee52565819205c25210c906b1618b427c7
Roberto <robertog.adm@gmail.com>
2023-05-08 18:51:01 -0300

    HAHAH

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,7 +10,7 @@ function Home() {
     <div>
       <Header />
       <div className="FirstSection">
-      <img src="src/assets/LogoNE.svg"  alt="Logo" />
+     {/*  <img src="src/assets/LogoNE.svg"  alt="Logo" /> */}
         <div className="FirstSectionInfo">
           <p>Consultoria Empresarial,<br/> Sistemas & Gráfica Rápida</p>
           <span>
@@ -25,21 +25,21 @@ function Home() {
         <h2>Nossos Serviços</h2>
         <div className="CardSection">
           <div className="Card">
-            <img src="src/assets/meeting-4156018_1280.jpg" alt="Job" />
+           {/*  <img src="src/assets/meeting-4156018_1280.jpg" alt="Job" /> */}
             <h3>Consultoria Empresarial</h3>
             <p>Plano de negócio, consultoria em marketing, consultoria financeira, consultoria de recursos humanos ...</p>
             <Link className="SkinButton">Ver Projetos</Link>
           </div>
 
           <div className="Card">
-            <img src="src/assets/printer-933098_1280.jpg" alt="Job" />
+           {/*  <img src="src/assets/printer-933098_1280.jpg" alt="Job" /> */}
             <h3>Gráfica Rápida</h3>
             <p>Cartão de visitas, TAGs, Imã de Geladeira, Banners, Faixas, Receituários, Blocos de pedidos, cardápios,Placas de sinalização PVC e muito mais.</p>
             <Link className="SkinButton">Ver Projetos</Link>
           </div>
 
           <div className="Card">
-            <img src="src/assets/WhatsApp Image 2023-05-06 at 10.23.38.jpeg" alt="Job" />
+          {/*   <img src="src/assets/WhatsApp Image 2023-05-06 at 10.23.38.jpeg" alt="Job" /> */}
             <h3>Projeto Foca Esporte</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora a reiciendis accusantium corporis amet assumenda. Debitis voluptate maiores ut ad voluptatibus, modi vero sit earum quod est atque neque. Aut?</p>
             <Link className="SkinButton">Ver Projetos</Link>
